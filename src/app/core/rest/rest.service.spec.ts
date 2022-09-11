bdc9ded68335c087700a88cab958146c4e45ed0b
bielzin <oliva.gabriel@outlook.com>
2022-09-11 17:48:39 -0300

    adds test to rest service

--- src/app/core/rest/rest.service.spec.ts
+++ src/app/core/rest/rest.service.spec.ts
@@ -1,16 +1,63 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RestService } from './rest.service';
+import { HttpClientTestingModule,
+  HttpTestingController } from '@angular/common/http/testing';
 
 describe('RestService', () => {
   let service: RestService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [RestService],
+      imports: [HttpClientTestingModule]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(RestService);
   });
 
+  afterEach(() => {
+    httpTestingController.verify();
+  })
+
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should make a get request', () => {
+    const url = 'http://www.test.com/';
+    service.get(url).subscribe();
+    const req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('GET')
+
+    req.flush({});
+  });
+
+  it('should make a post request', () => {
+    const url = 'http://www.test.com/';
+    service.post(url, {}).subscribe();
+    const req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('POST')
+
+    req.flush({});
+  });
+
+  it('should make a put request', () => {
+    const url = 'http://www.test.com/';
+    service.put(url, {}).subscribe();
+    const req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('PUT')
+
+    req.flush({});
+  });
+
+  it('should make a delete request', () => {
+    const url = 'http://www.test.com/';
+    service.delete(url).subscribe();
+    const req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('DELETE')
+
+    req.flush({});
   });
 });
